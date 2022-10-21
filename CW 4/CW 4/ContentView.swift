dd974dcef2ded56e7f22e1b20fce4141d9e1ebec
Bo3lii <aoosa.alhaddad1@gmail.com>
2022-10-21 13:27:17 +0300

    hussain

--- CW 4/CW 4/ContentView.swift
+++ CW 4/CW 4/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     
     @State var grade = ""
-    
+    @State var a = ""
     var body: some View {
         ZStack{
             Color.cyan.ignoresSafeArea()
@@ -25,10 +25,52 @@ struct ContentView: View {
                 Image("calc")
                     .resizable()
                     .scaledToFit()
+                    .frame(width: 300 , height: 300)
                     .padding()
                     
                 
                     TextField(" Enter your grade ", text : $grade)
+                    .font(.system(size: 30))
+                    .background(Color.white)
+                    .frame(width: 350)
+                    .cornerRadius(10)
+                    .padding()
+                    
+                
+                
+                
+                Text("Calcualate my grade")
+                    .font(.system(size: 30))
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.yellow)
+                    .cornerRadius(30)
+                   
+                    .onTapGesture {
+                        
+                        if Int(grade) ?? 0 >= 90 {
+                            a = "A+"
+                            
+                        }else if Int(grade) ?? 0 >= 80 {
+                            a = "B"
+                            
+                        }else if Int(grade) ?? 0 >= 70 {
+                            a = "C"
+                            
+                        }else if Int(grade) ?? 0 >= 60 {
+                            a = "D"
+                            
+                        }else{
+                            a = "F"
+                        }
+                       
+                        
+                            
+                    }
+                Text(a)
+                    .font(.system(size: 40,design: .serif))
+                    .foregroundColor(.white)
+                
                     
                 
                 
